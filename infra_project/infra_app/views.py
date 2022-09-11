b2cd3a008265bd3f923206254865c334d9968d3f
Denis <deni_95@list.ru>
2022-09-11 22:33:53 +0400

    s

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,11 +1,9 @@
-from urllib import response
-
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(HttpResponse, 'У меня получилось!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse(HttpResponse, 'А это вторая страница')
+    return HttpResponse('А это вторая страница')
